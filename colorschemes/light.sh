effcc407a6a195ca777939cd0aa72a0c81c2fc97
pukkamustard <pukkamustard@posteo.net>
2017-09-27 22:05:37 +0200

    Add Atom One inspired dark and light colorschemes

---
+++ colorschemes/light.sh
@@ -0,0 +1,28 @@
+# light (based on Atom's one-light)
+
+background="#F9F9F9"
+foreground="#383A42"
+
+cursor="#383A42"
+mouse_background="#F9F9F9"
+mouse_foreground="#383A42"
+
+highlight="#3A3F4B"
+border="#F9F9F9"
+
+color0="#000000" # black
+color1="#E45649" # red
+color2="#50A14F" # green
+color3="#986801" # yellow
+color4="#4078F2" # blue
+color5="#A626A4" # magenta
+color6="#0184BC" # cyan
+color7="#A0A1A7" # white
+color8="#383A42" # brblack
+color9="#E45649" # brred
+color10="#50A14F" # brgreen
+color11="#986801" # bryellow
+color12="#4078F2" # brblue
+color13="#A626A4" # brmagenta
+color14="#0184BC" # brcyan
+color15="#FFFFFF" # brwhite
